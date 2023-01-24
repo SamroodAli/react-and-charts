b1c7dbe096618dce9e768a18a8e6b4811a896dd8
Samrood Ali <samrood@openvitae.io>
2023-01-24 22:48:34 +0530

    feat: render data from api to a bar char

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,60 @@ import { PageLayout } from "@/components/PageLayouts";
 import Head from "next/head";
 import useSWR from "swr";
 import { fetcher } from "@/utils/api";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function Home() {
   const { data, error, isLoading } = useSWR("/data", fetcher);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+
+  if (error) {
+    return <div>error</div>;
+  }
+
+  const dataSet = {
+    labels: data.data.map((each: any) => each.id),
+    datasets: [
+      {
+        label: "dataset-1",
+        data: data.data.map((stock: any) => stock.price),
+        backgroundColor: "red",
+      },
+    ],
+  };
 
   return (
     <>
@@ -14,7 +65,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageLayout>hello</PageLayout>
+      <PageLayout>
+        <Bar data={dataSet} />
+      </PageLayout>
     </>
   );
 }
